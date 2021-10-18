4e9f09d246ba7072501e28c316cd5b3a3509bbf2
unknown <rahmanmahadi966@gmail.com>
2021-10-18 18:15:06 +0600

    firebase

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBditqcXrRb9FUkvGBJlu1nSjQTTo_RZFc",
+    authDomain: "yoga-burn-8680c.firebaseapp.com",
+    projectId: "yoga-burn-8680c",
+    storageBucket: "yoga-burn-8680c.appspot.com",
+    messagingSenderId: "987801730448",
+    appId: "1:987801730448:web:39edeb13ceac921778c715"
+  };
+
+  export default firebaseConfig;
